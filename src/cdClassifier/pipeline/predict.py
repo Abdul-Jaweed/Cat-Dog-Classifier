ba9584572317e652cd4dcbf0dd8afd885e7428c2
Abdul Jaweed <jdgaming7320@gmail.com>
2023-05-15 12:09:04 +0530

    prediction pipeline and flast web ui added

--- src/cdClassifier/pipeline/predict.py
+++ src/cdClassifier/pipeline/predict.py
@@ -1,45 +1,27 @@
-from flask import Flask, request, jsonify, render_template
+import numpy as np
+from tensorflow.keras.models import load_model
+from tensorflow.keras.preprocessing import image
 import os
-from flask_cors import CORS, cross_origin
-from cdClassifier.utils import decodeImage
-from cdClassifier.pipeline.predict import DogCat
 
-os.putenv('LANG', 'en_US.UTF-8')
-os.putenv('LC_ALL', 'en_US.UTF-8')
+class DogCat:
+    def __init__(self,filename):
+        self.filename =filename
 
-app = Flask(__name__)
-CORS(app)
+    def predictiondogcat(self):
+        # load model
+        model = load_model(os.path.join("artifacts","training", "model.h5"))
 
+        imagename = self.filename
+        test_image = image.load_img(imagename, target_size = (224,224))
+        test_image = image.img_to_array(test_image)
+        test_image = np.expand_dims(test_image, axis = 0)
+        result = np.argmax(model.predict(test_image), axis=1)
+        print(result)
 
-class ClientApp:
-    def __init__(self):
-        self.filename = "inputImage.jpg"
-        self.classifier = DogCat(self.filename)
+        if result[0] == 1:
+            prediction = 'dog'
+            return [{ "image" : prediction}]
+        else:
+            prediction = 'cat'
+            return [{ "image" : prediction}]
 
-
-@app.route("/", methods=['GET'])
-@cross_origin()
-def home():
-    return render_template('index.html')
-
-
-@app.route("/train", methods=['GET','POST'])
-@cross_origin()
-def trainRoute():
-    os.system("python main.py")
-    return "Training done successfully!"
-
-
-
-@app.route("/predict", methods=['POST'])
-@cross_origin()
-def predictRoute():
-    image = request.json['image']
-    decodeImage(image, clApp.filename)
-    result = clApp.classifier.predictiondogcat()
-    return jsonify(result)
-
-
-if __name__ == "__main__":
-    clApp = ClientApp()
-    app.run(host='0.0.0.0', port=8080)
